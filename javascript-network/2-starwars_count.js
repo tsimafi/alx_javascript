ccece74a783a53edc9e2f5051d5eccd9f875d580
tsion mesfin <sionmesfin@gmail.com>
2024-02-13 18:23:41 +0300

    new file

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,32 +1,30 @@
-#!/usr/bin/node
-const request = require('request');
+import sys
+import requests
 
-// Retrieve the API URL from the command line argument
-const apiUrl = process.argv[2];
-
-// Wedge Antilles character ID
-const WEDGE_ANTILLES_ID = '18';
-
-// Make the GET request to the Star Wars API
-request.get(apiUrl, (error, response, body) => {
-    if (error) {
-        // Handle error if request fails
-        console.error(error);
-    } else {
-        // Parse the JSON response body
-        const films = JSON.parse(body).results;
+def count_movies_with_character(api_url):
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+        films_data = response.json()['results']
         
-        // Initialize a counter for movies with Wedge Antilles
-        let movieCount = 0;
+        # Count movies where "Wedge Antilles" is present
+        count = 0
+        for film in films_data:
+            characters = film['characters']
+            if 'https://swapi-api.alx-tools.com/api/people/18/' in characters:
+                count += 1
+        
+        return count
 
-        // Iterate through the films to count occurrences of Wedge Antilles
-        films.forEach(film => {
-            if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${WEDGE_ANTILLES_ID}/`)) {
-                movieCount++;
-            }
-        });
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
+        return -1
 
-        // Print the number of movies where Wedge Antilles is present
-        console.log(movieCount);
-    }
-});
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <API_URL>")
+        sys.exit(1)
+    
+    api_url = sys.argv[1]
+    movies_with_wedge = count_movies_with_character(api_url)
+    print(movies_with_wedge)
