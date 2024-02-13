0f3777198a8f83d71054490d2781dac7469cb1b5
tsion mesfin <sionmesfin@gmail.com>
2024-02-13 17:58:36 +0300

    new file

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,4 @@
+// Use console.log(...) to print all output
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+// Define an array of strings
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// Iterate through the array using a loop
+for (let i = 0; i < languages.length; i++) {
+    // Use console.log to print each string in the array
+    console.log(languages[i]);
+}
