b68292c90e1b4a2af828215e13f6e5b4201bf01a
tsion mesfin <sionmesfin@gmail.com>
2024-02-13 17:43:34 +0300

    new file

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,16 +1,12 @@
-// 2-rectangle.js
 class Rectangle {
     constructor(w, h) {
         if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-            // If w or h is equal to 0 or not a positive integer, create an empty object
-            this.width = undefined;
-            this.height = undefined;
-        } else {
-            // Initialize the instance attribute width with the value of w
-            this.width = w;
-            // Initialize the instance attribute height with the value of h
-            this.height = h;
+            // If width or height is not a positive integer or is NaN, create an empty object
+            return {};
         }
+        // Initialize width and height attributes
+        this.width = w;
+        this.height = h;
     }
 }
 
