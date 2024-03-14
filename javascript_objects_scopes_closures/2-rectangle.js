addce137ded95f955bdc2bac4498f215e1966f93
tsion mesfin <sionmesfin@gmail.com>
2024-03-14 11:26:37 +0300

    new

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,26 @@
-class Rectangle {
-    constructor(w, h) {
-        if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-            // If width or height is not a positive integer or is NaN, create an empty object
-            return {};
-        }
-        // Initialize width and height attributes
-        this.width = w;
-        this.height = h;
-    }
-}
+#!/usr/bin/python3
+import MySQLdb
+import sys
 
-module.exports = Rectangle;
+if __name__ == "__main__":
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    # Create a cursor object using cursor() method
+    cursor = db.cursor()
+
+    # Construct SQL query using format with user input
+    sql = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4])
+
+    # Execute SQL query
+    cursor.execute(sql)
+
+    # Fetch all the rows in a list of lists
+    results = cursor.fetchall()
+
+    # Display results
+    for row in results:
+        print(row)
+
+    # Disconnect from server
+    db.close()
